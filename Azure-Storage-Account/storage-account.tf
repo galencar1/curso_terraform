f8876bce0a0f28bf5d11cbe20d844fe39386f70e
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-05 08:44:24 -0300

    Add variables accout tier and accout replication type

--- Azure-Storage-Account/storage-account.tf
+++ Azure-Storage-Account/storage-account.tf
@@ -10,8 +10,8 @@ resource "azurerm_storage_account" "first_storage_account" {
   name = "" // Vazio - Por enquanto
   resource_group_name = "" // Vazio - Por enquanto
   location = var.location // Será chamado através de uma variável do arquivo "Variables.tf"
-  account_tier = "" // Vazio - Por enquanto
-  account_replication_type = "" // Vazio - Por enquanto
+  account_tier = var.account_tier // Será chamado através de uma variável do arquivo "Variables.tf"
+  account_replication_type = var.account_replication_type // Será chamado através de uma variável do arquivo "Variables.tf"
 
   tags = "" // Vazio - Por enquanto
 }
