88cd336ace272b4ee0862615fc6118f3404bc40c
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-05 08:24:45 -0300

    Add Variable Location

--- Azure-Storage-Account/storage-account.tf
+++ Azure-Storage-Account/storage-account.tf
@@ -1,8 +1,7 @@
 // Primeiro  -Criar um resource group
 resource "azurerm_resource_group" "first_resource_group" {
   name = "" // Vazio - Por enquanto
-  location = "" // Vazio - Por enquanto
-
+  location = var.location // Será chamado através de uma variável do arquivo "Variables.tf"
   tags = "" // Vazio - Por enquanto
 }
 
@@ -10,7 +9,7 @@ resource "azurerm_resource_group" "first_resource_group" {
 resource "azurerm_storage_account" "first_storage_account" {
   name = "" // Vazio - Por enquanto
   resource_group_name = "" // Vazio - Por enquanto
-  location = "" // Vazio - Por enquanto
+  location = var.location // Será chamado através de uma variável do arquivo "Variables.tf"
   account_tier = "" // Vazio - Por enquanto
   account_replication_type = "" // Vazio - Por enquanto
 
---
+++ Variables/Variables.tf
@@ -0,0 +1,5 @@
+variable "location" {
+  description = "variável que indica a região onde os recursos serão criados." // Descrição da função da variável
+  type = "string" // Tipo da variável
+  default = "Brazil South" //indica a Região em que o recurso será inicializado
+}
