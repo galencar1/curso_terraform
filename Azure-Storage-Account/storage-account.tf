682ea2460f6c00e035b0fad4da89004509567f61
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-05 08:58:12 -0300

    Referencing attributes from other blocks

--- Azure-Storage-Account/storage-account.tf
+++ Azure-Storage-Account/storage-account.tf
@@ -1,14 +1,14 @@
 // Primeiro  -Criar um resource group
 resource "azurerm_resource_group" "first_resource_group" {
-  name = "" // Vazio - Por enquanto
+  name = "storage_account_resource_group" // Nome do resource group para ser referenciado em outros blocos
   location = var.location // Será chamado através de uma variável do arquivo "Variables.tf"
   tags = "" // Vazio - Por enquanto
 }
 
 // Segundo - Criar um storage Account
 resource "azurerm_storage_account" "first_storage_account" {
-  name = "" // Vazio - Por enquanto
-  resource_group_name = "" // Vazio - Por enquanto
+  name = "gabrielalencarstorageaccount" // Nome da Storage Account
+  resource_group_name = azurerm_resource_group.first_resource_group.name // Referência ao Resource Group
   location = var.location // Será chamado através de uma variável do arquivo "Variables.tf"
   account_tier = var.account_tier // Será chamado através de uma variável do arquivo "Variables.tf"
   account_replication_type = var.account_replication_type // Será chamado através de uma variável do arquivo "Variables.tf"
@@ -18,5 +18,5 @@ resource "azurerm_storage_account" "first_storage_account" {
 
 resource "azurerm_storage_container" "first_container" {
   name = "" // Vazio - Por enquanto
-  storage_account_name = ""  // Vazio - Por enquanto
+  storage_account_name = azurerm_storage_account.first_storage_account.name  // Referencia da storage account
 }
