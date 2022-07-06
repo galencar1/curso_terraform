6e00dbedb9bd47e5a56ec15850b438a40f945896
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-06 11:54:48 -0300

    configure main.tf

---
+++ Aws-Vm-Vpc-Remote-State/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "3.73.0   "
+    }
+  }
+
+  backend "s3" {
+    bucket = "gabrielfa-remote-state"
+    key = "aws-vm/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+    owner = "Gabriel Alencar"
+    managed-by = "terraform"
+    }
+  }
+}
+
+//Pega os dados que foram feitos output na VPC. Por isso as informações devem ser iguais
+// as informações adicionadas na backend-vpc no main.tf
+data "terraform-remote-state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "gabrielfa-remote-state"
+    key = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
