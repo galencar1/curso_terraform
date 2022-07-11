aeafdc2b20a479ed373bc345cbad012802ae88d1
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-11 16:27:01 -0300

    Conditional Expressions

---
+++ Conditionals-Expressions-Aws/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "gabrielfa-remote-state"
+    key    = "aws-vpc-conditionals/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "Gabriel Alencar"
+      managed-by = "terraform"
+    }
+  }
+}
