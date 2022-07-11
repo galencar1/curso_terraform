0f0e22b6542c9f59aca7349ff7c7d463690786d5
Gabriel Alencar <gfalencar02@gmail.com>
2022-07-11 16:13:58 -0300

    Lifecycle hooks

---
+++ Lifecycle-aws-bucket/bucket.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "lifecycle" {
+  bucket = "gabrielfa-lifecycle"
+
+  lifecycle {
+    #create_before_destroy = true
+    #prevent_destroy = true
+    ignore_changes = [
+     tags, 
+    ]
+  }
+
+  tags = {
+    Aula = "Lifecycle"
+  }
+}
